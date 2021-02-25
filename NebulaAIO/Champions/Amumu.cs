edc9b4a6302631941b09c827e813adf529921df9
Senthixx <33396951+Senthixx@users.noreply.github.com>
2021-02-25 08:34:34 +0100

    Added Alistar & Amumu

---
+++ NebulaAIO/Champions/Amumu.cs
@@ -0,0 +1,274 @@
+﻿using EnsoulSharp;
+using EnsoulSharp.SDK;
+using EnsoulSharp.SDK.MenuUI;
+using SharpDX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Security.Policy;
+
+namespace NebulaAio.Champions
+{
+
+    public class Amumu
+    {
+        private static Spell Q, W, E, R;
+        private static Menu Config;
+
+        public static void OnGameLoad()
+        {
+            if (ObjectManager.Player.CharacterName != "Amumu")
+            {
+                return;
+            }
+
+            Q = new Spell(SpellSlot.Q, 1100f);
+            W = new Spell(SpellSlot.W, 300f);
+            E = new Spell(SpellSlot.E, 350f);
+            R = new Spell(SpellSlot.R, 550f);
+
+            Q.SetSkillshot(0.25f, 160f, 2000f, true, SpellType.Line);
+
+
+
+            Config = new Menu("Amumu", "[Nebula]: Amumu", true);
+
+            var menuC = new Menu("Csettings", "Combo");
+            menuC.Add(new MenuBool("UseQ", "Use Q in Combo"));
+            menuC.Add(new MenuBool("UseW", "Use W in Combo"));
+            menuC.Add(new MenuBool("UseE", "Use E in Combo"));
+            menuC.Add(new MenuBool("UseR", "Use R in Combo"));
+            menuC.Add(new MenuSlider("rcount", "Min enemys To use R", 2, 1, 5));
+
+            var menuL = new Menu("Clear", "Clear");
+            menuL.Add(new MenuBool("LcE", "Use E in Lanclear"));
+            menuL.Add(new MenuBool("JcQ", "Use Q in Jungleclear"));
+            menuL.Add(new MenuBool("JcE", "Use E in Jungleclear"));
+
+            var menuM = new Menu("Misc", "Misc");
+            menuM.Add(new MenuBool("AW", "Auto turn off W"));
+            
+            var menuK = new Menu("Killsteal", "Killsteal");
+            menuK.Add(new MenuBool("KsQ", "Use Q to Killsteal"));
+            menuK.Add(new MenuBool("KsE", "Use E to Killsteal"));
+            menuK.Add(new MenuBool("KsR", "Use R to Killsteal", false));
+
+            var menuD = new Menu("dsettings", "Drawings ");
+            menuD.Add(new MenuBool("drawQ", "Q Range  (White)", true));
+            menuD.Add(new MenuBool("drawW", "W Range  (White)", true));
+            menuD.Add(new MenuBool("drawE", "E Range (White)", true));
+            menuD.Add(new MenuBool("drawR", "R Range  (Red)", true));
+
+
+
+            Config.Add(menuC);
+            Config.Add(menuL);
+            Config.Add(menuM);
+            Config.Add(menuK);
+            Config.Add(menuD);
+
+            Config.Attach();
+
+            GameEvent.OnGameTick += OnGameUpdate;
+            Drawing.OnDraw += OnDraw;
+        }
+
+        public static void OnGameUpdate(EventArgs args)
+        {
+
+            if (Config["Misc"].GetValue<MenuBool>("AW").Enabled)
+            {
+                AutoW();
+            }
+
+            if (Orbwalker.ActiveMode == OrbwalkerMode.Combo)
+            {
+                LogicQ();
+                LogicW();
+                LogicE();
+                LogicR();
+            }
+
+            if (Orbwalker.ActiveMode == OrbwalkerMode.LaneClear)
+            {
+                Laneclear();
+                Jungle();
+            }
+
+            if (Orbwalker.ActiveMode == OrbwalkerMode.LastHit)
+            {
+
+            }
+
+            if (Orbwalker.ActiveMode == OrbwalkerMode.Harass)
+            {
+
+            }
+            Killsteal();
+        }
+
+        private static void AutoW()
+        {
+            var minions = GameObjects.GetMinions(W.Range + 50);
+            var mobs = GameObjects.GetJungles(W.Range + 50);
+            
+            if (ObjectManager.Player.HasBuff("AuraofDespair"))
+            {
+                if (ObjectManager.Player.CountEnemyHeroesInRange(W.Range + 50) == 0 && (minions.Count() == 0 && mobs.Count() == 0))
+                {
+                    W.Cast();
+                }
+            }
+        }
+
+        private static void OnDraw(EventArgs args)
+        {
+            if (Config["dsettings"].GetValue<MenuBool>("drawQ").Enabled)
+            {
+                Render.Circle.DrawCircle(ObjectManager.Player.Position, Q.Range, System.Drawing.Color.White);
+            }
+            
+            if (Config["dsettings"].GetValue<MenuBool>("drawW").Enabled)
+            {
+                Render.Circle.DrawCircle(ObjectManager.Player.Position, W.Range, System.Drawing.Color.White);
+            }
+
+            if (Config["dsettings"].GetValue<MenuBool>("drawE").Enabled)
+            {
+                Render.Circle.DrawCircle(ObjectManager.Player.Position, E.Range, System.Drawing.Color.White);
+            }
+
+            if (Config["dsettings"].GetValue<MenuBool>("drawR").Enabled)
+            {
+                Render.Circle.DrawCircle(ObjectManager.Player.Position, R.Range, System.Drawing.Color.Red);
+            }
+        }
+
+        private static void LogicR()
+        {
+            var target = TargetSelector.GetTarget(1000);
+            var useR = Config["Csettings"].GetValue<MenuBool>("UseR");
+            var combor = Config["Csettings"].GetValue<MenuSlider>("rcount").Value;
+            if (target == null) return;
+            
+            if (R.IsReady() && useR.Enabled && Q.GetDamage(target) + E.GetDamage(target) + R.GetDamage(target) >= target.Health && target.IsValidTarget(R.Range))
+            {
+                R.Cast();
+            }
+            
+            if (R.IsReady() && useR.Enabled && ObjectManager.Player.CountEnemyHeroesInRange(R.Range) >= combor)
+            {
+                R.Cast();
+            }
+        }
+
+        private static void LogicW()
+        {
+            var target = TargetSelector.GetTarget(W.Range);
+            var useW = Config["Csettings"].GetValue<MenuBool>("UseW");
+            if (target == null) return;
+
+            if (W.IsReady() && useW.Enabled &&  target.IsValidTarget(W.Range))
+            {
+                if (ObjectManager.Player.HasBuff("AuraofDespair") &&
+                    GameObjects.EnemyHeroes.Count(x => x.DistanceToPlayer() > W.Range) ==
+                    GameObjects.EnemyHeroes.Count())
+                {
+                    W.Cast();
+                }
+                if (target.DistanceToPlayer() < W.Range - 60 && !ObjectManager.Player.HasBuff("AuraofDespair"))
+                {
+                    W.Cast();
+                }
+            }
+        }
+
+        private static void LogicE()
+        {
+            var target = TargetSelector.GetTarget(E.Range);
+            var useE = Config["Csettings"].GetValue<MenuBool>("UseE");
+            if (target == null) return;
+
+            if (E.IsReady() && useE.Enabled && target.IsValidTarget(E.Range))
+            {
+                E.Cast();
+            }
+        }
+        
+
+        private static void LogicQ()
+        {
+            var target = TargetSelector.GetTarget(1000);
+            var useQ = Config["Csettings"].GetValue<MenuBool>("UseQ");
+            var input = Q.GetPrediction(target, false);
+            if (target == null) return;
+
+            if (Q.IsReady() && useQ.Enabled && target.IsValidTarget(Q.Range))
+            {
+                if (input.Hitchance >= HitChance.High)
+                {
+                    Q.Cast(input.UnitPosition);
+                }
+            }
+        }
+
+        private static void Jungle()
+        {
+            var JcEe = Config["Clear"].GetValue<MenuBool>("JcE");
+            var JcQq = Config["Clear"].GetValue<MenuBool>("JcQ");
+            var mobs = GameObjects.Jungle.Where(x => x.IsValidTarget(E.Range)).OrderBy(x => x.MaxHealth)
+                .ToList<AIBaseClient>();
+            if (mobs.Count > 0)
+            {
+                var mob = mobs[0];
+                if (JcEe.Enabled && E.IsReady() && ObjectManager.Player.Distance(mob.Position) < E.Range) E.Cast(mob);
+                if (JcQq.Enabled && Q.IsReady() &&  ObjectManager.Player.Distance(mob.Position) < Q.Range) Q.Cast(mob);
+            }
+        }
+
+
+        private static void Laneclear()
+        {
+            var lce = Config["Clear"].GetValue<MenuBool>("LcE");
+            if (lce.Enabled && E.IsReady())
+            {
+                var minions = GameObjects.EnemyMinions.Where(x => x.IsValidTarget(E.Range) && x.IsMinion())
+                    .Cast<AIBaseClient>().ToList();
+                if (minions.Any())
+                {
+                    var eFarmLocation = E.GetCircularFarmLocation(minions);
+                    if (eFarmLocation.Position.IsValid())
+                    {
+                        E.Cast(eFarmLocation.Position);
+                        return;
+                    }
+                }
+            }
+        }
+
+        private static void Killsteal()
+        {
+            var ksQ = Config["Killsteal"].GetValue<MenuBool>("KsQ").Enabled;
+            var ksR = Config["Killsteal"].GetValue<MenuBool>("KsR").Enabled;
+            var ksE = Config["Killsteal"].GetValue<MenuBool>("KsE").Enabled;
+            var target = TargetSelector.GetTarget(1000);
+
+            if (target == null) return;
+            if (target.IsInvulnerable) return;
+
+            if (!(ObjectManager.Player.Distance(target.Position) <= Q.Range) ||
+                !(ObjectManager.Player.GetSpellDamage(target, SpellSlot.Q) >= target.Health + 20)) return;
+            if (Q.IsReady() && ksQ) Q.Cast(target);
+            
+            if (!(ObjectManager.Player.Distance(target.Position) <= R.Range) ||
+                !(ObjectManager.Player.GetSpellDamage(target, SpellSlot.R) >= target.Health + 20)) return;
+            if (R.IsReady() && ksR) R.Cast(target);
+            
+            if (!(ObjectManager.Player.Distance(target.Position) <= E.Range) ||
+                !(ObjectManager.Player.GetSpellDamage(target, SpellSlot.E) >= target.Health + 20)) return;
+            if (E.IsReady() && ksE) E.Cast(target);
+            
+        }
+    }
+}
--- NebulaAIO/Program.cs
+++ NebulaAIO/Program.cs
@@ -126,6 +126,18 @@ namespace NebulaAio
                         Game.Print("<font color='#ff0000' size='25'> [NebulaAIO]:  </font>" + "<font color='#F7FF00' size='25'>Devloped By Akane </font>");
                         break;
                     
+                    case "Amumu":
+                        Amumu.OnGameLoad();
+                        Game.Print("<font color='#ff0000' size='25'> [NebulaAIO]:  </font>" + ObjectManager.Player.CharacterName + " Loaded");
+                        Game.Print("<font color='#ff0000' size='25'> [NebulaAIO]:  </font>" + "<font color='#F7FF00' size='25'>Devloped By Akane </font>");
+                        break;
+                    
+                    case "Alistar":
+                        Alistar.OnGameLoad();
+                        Game.Print("<font color='#ff0000' size='25'> [NebulaAIO]:  </font>" + ObjectManager.Player.CharacterName + " Loaded");
+                        Game.Print("<font color='#ff0000' size='25'> [NebulaAIO]:  </font>" + "<font color='#F7FF00' size='25'>Devloped By Akane </font>");
+                        break;
+                    
                     default:
                         Game.Print("<font color='#ff0000' size='25'>[NebulaAIO] Does Not Support :" + ObjectManager.Player.CharacterName + "</font>");
                         Console.WriteLine("[NebulaAIO] Does Not Support " + ObjectManager.Player.CharacterName);
@@ -139,7 +151,7 @@ namespace NebulaAio
                     ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
                     stringg = client.DownloadString(uri);
                 }
-                string versionas = "2.0.0.14\n";
+                string versionas = "2.0.0.15\n";
                 if (versionas != stringg)
                 {
                     Game.Print("<font color='#ff0000'> [NebulaAIO]: </font> <font color='#ffe6ff' size='25'>You don't have the current version, please UPDATE !</font>");
